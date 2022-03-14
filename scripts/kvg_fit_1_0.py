2eb1d6d715c97255ca6660c8c90dfba80fe596da
jediar <jediar@yandex.ru>
2022-03-14 11:01:31 +0300

    Добавление README.md + вывод матрицы ошибок в fit

--- scripts/kvg_fit_1_0.py
+++ scripts/kvg_fit_1_0.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.ensemble import IsolationForest, RandomForestClassifier
 import sys
 from joblib import dump
+from sklearn.metrics import confusion_matrix
 
 def fit_save(train, test):
 	columns = [c for c in train.columns if c != 'quality'];
@@ -14,8 +15,14 @@ def fit_save(train, test):
 
 	model = RandomForestClassifier(n_estimators=300, max_depth=15, random_state=23)
 	model.fit(X_train, y_train)
+	model.score
 
 	dump(model, './model.joblib')
+	
+	y_pred = model.predict(X_test)
+	score = model.score(X_test, y_test)
+	print(f'Accuracy: {score}')
+	print(confusion_matrix(y_test, y_pred))
 
 if __name__ == '__main__':
 	train = pd.read_csv(sys.argv[1])
